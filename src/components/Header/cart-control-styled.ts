672997e7dbcacda591c7f94e7c271e3ac4603410
kleuton novais <106082564+kleutons@users.noreply.github.com>
2023-05-28 19:18:40 -0300

    feat: Creat Header

---
+++ src/components/Header/cart-control-styled.ts
@@ -0,0 +1,21 @@
+"use client"
+
+import styled from "styled-components";
+
+export const CartCount = styled.span`
+    border-radius: 100%;
+    padding: 0 5px;
+    font-size: 10px;
+    background-color: var(--delete-color);
+    color: white;
+    margin-left: -10px;
+    margin-bottom: -10px;
+    display: flex;
+    align-items: center;
+`
+
+export const Container = styled.div`
+    display: flex;
+    align-items: baseline;
+    cursor: pointer;
+`
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,28 @@
+import { HeaderSearchInput } from '../Inputs/header-search';
+import { CartControl } from './cart-control';
+import * as S from './header-styled';
+
+import { Saira_Stencil_One } from 'next/font/google'
+
+const sairaStencil = Saira_Stencil_One({ 
+  weight: ['400'],
+  subsets: ['latin']
+ })
+
+
+interface HeaderProps{
+
+}
+
+
+export function Header(props:HeaderProps){
+    return(
+        <S.TagHeader>
+            <S.Logo className={sairaStencil.className}> coffee </S.Logo>
+            <div>
+                <HeaderSearchInput placeholder='Procurando por algo específico?' />
+                <CartControl />
+            </div>
+        </S.TagHeader>
+    )
+}
